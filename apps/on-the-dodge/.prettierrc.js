2e8a89b1ddd79ebfc4fd1f28281ebecaafb55146
WillSnake <willsnake87@gmail.com>
2022-09-25 21:52:02 -0500

    init-on-the-dodge

---
+++ apps/on-the-dodge/.prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    require("prettier-plugin-tailwindcss"),
+    require("prettier-plugin-astro")
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: { parser: "astro" }
+    }
+  ],
+  trailingComma: "none",
+  semi: false,
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2
+}
